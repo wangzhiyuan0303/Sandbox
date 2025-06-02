a3c19ac620c184cf75ebc8ada79842421c72d8e2
wang <wangoffer0902@163.com>
2025-06-02 13:33:00 +0800

    abc

---
+++ test1.py
@@ -0,0 +1,25 @@
+filename = input("Enter the filename: ")
+
+with open(filename, 'r') as file:
+    for line in file:
+        if line.startswith('#'):
+            print(line, end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
